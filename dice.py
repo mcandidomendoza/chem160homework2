8400d02f27a99c2a4512ad6c868e4aa62feb073d
mcandidomendoza <70285819+mcandidomendoza@users.noreply.github.com>
2020-09-13 16:09:12 -0700

    Add files via upload

--- dice.py
+++ dice.py
@@ -3,13 +3,12 @@ ntrials=15000
 player1wins=0
 for i in range(ntrials):
     dice2=choices(range(1,7),k=2)
-    if dice2==dice2:
+    if dice2[0]==dice2[1]:
             continue
-    total2=total2+dice2
-for i in range(ntrials):
+    total2=dice2[0]+dice2[1]
     dice1=choices(range(1,7),k=3)
     dice1.sort(reverse=True)
-    total1=total1+dice1[0]+dice1[1]
+    total1=dice1[0]+dice1[1]
     if total1 > total2:
             player1wins+=1
 print("Ratio of Player 1 winning", player1wins/ntrials)
